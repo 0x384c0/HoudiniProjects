38f50ee06ce79573b030c47953b5de51e22966c6
0x384c0 <0x384c0@gmail.com>
2025-02-27 22:29:02 +0600

    added magical fx

---
+++ R&D/magical_fx/realflare/render.ps1
@@ -0,0 +1,7 @@
+python -m realflare `
+  --project "af_s_28_70mm_white_anamorphic.json" `
+  --animation "realflare_animation.json" `
+  --frame-start 1 `
+  --frame-end 20 `
+  --element FLARE `
+  --output output/render.%04d.exr
